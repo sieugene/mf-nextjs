df188d1894327e50ddd0dbeacd78850a40077cdf
sieugene <sieugene@mail.ru>
2021-08-22 21:00:50 +0300

    ci : fix app env mode

--- utils/env.installer.js
+++ utils/env.installer.js
@@ -4,12 +4,12 @@ const envProd = require("../.env.production.js");
 module.exports = {
   EnvInstaller: (process) => {
     const APP_ENV =
-      process.env.APP_ENV === "development" ? "development" : "production";
+      process.env.NODE_ENV === "development" ? "development" : "production";
 
-    // if (APP_ENV === "development") {
-    //   return { APP_ENV, ...envDev };
-    // } else {
-    return { APP_ENV, ...envProd };
-    // }
+    if (APP_ENV === "development") {
+      return { APP_ENV, ...envDev };
+    } else {
+      return { APP_ENV, ...envProd };
+    }
   },
 };
