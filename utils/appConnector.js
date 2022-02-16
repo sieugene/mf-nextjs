c58642568accb37176a13f6b8aee297b22bd9ad3
sieugene <sieugene@mail.ru>
2022-02-16 22:02:00 +0300

    fix : path

--- utils/appConnector.js
+++ utils/appConnector.js
@@ -1,23 +1,23 @@
 const path = require("path");
+const appRoot = require("app-root-path");
 
 module.exports = {
   /**
    * Connecting an application in next config, takes in development mode and in build mode
    *
-   * @param {string} name - первое число
-   * @param {string} entry - второе число
+   * @param {string} name - name app
+   * @param {string} entry - entry app
    * @param {boolean} isServer
    * @param {boolean} isBuild
    * @returns string
    */
   appConnector(name, entry, isServer, isBuild) {
     if (isServer) {
-      return path.resolve(
-        __dirname,
-        `../${name}/.next/server/${
-          isBuild ? "chunks/static" : "static"
-        }/runtime/${entry}.js`
-      );
+      const paths = `${appRoot}/${name}/.next/server/${
+        isBuild ? "chunks/static" : "static"
+      }/runtime/${entry}.js`;
+
+      return paths;
     } else {
       return name;
     }
